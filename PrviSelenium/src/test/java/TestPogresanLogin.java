8a872e473157a1d79194e6c43cd55bc8613088e3
Amela Kadric <ka200374@rti.lab>
2022-11-09 16:46:41 +0100

    added test for wrong pass

--- PrviSelenium/src/test/java/TestPogresanLogin.java
+++ PrviSelenium/src/test/java/TestPogresanLogin.java
@@ -15,18 +15,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
-public class NewSeleneseIT {
+public class TestPogresanLogin {
     WebDriver driver;
     
     @Test
     public void testSimple() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\TEMP\\chromedriver.exe");
-        
-        driver = new ChromeDriver();
-        String baseUrl="https://www.saucedemo.com";
-        
-        driver.get(baseUrl);
-        driver.manage().window().maximize();
         
         WebDriverWait cekaj = new WebDriverWait(driver, 10);
         
@@ -37,21 +30,34 @@ public class NewSeleneseIT {
         
         //explicitni wait
         WebElement username = cekaj.until(ExpectedConditions.elementToBeClickable(By.id("user-name")));
+        username.clear();
         username.sendKeys("standard_user");
         
         WebElement psw = driver.findElement(By.id("password"));
-        psw.sendKeys("secret_sauce");
+        psw.clear();
+        psw.sendKeys("aaa");
         
         WebElement dugme = cekaj.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='login-button']")));
         dugme.click();
         Thread.sleep(5000);
         
-        String welcomeText = driver.findElement(By.xpath("//span[@class='title']")).getText();
-        Assert.assertEquals("PRODUCTS", welcomeText);
+        WebElement errorMsg=cekaj.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[@data-test='error']")));
+        
+        String errTxt = errorMsg.getText();
+      
+        Assert.assertTrue(errTxt.contains("Epic sadface"));
+        
+//        String expErrText="Epic sadface: Username and password do not match any user in this service";
+//        Assert.assertEquals(expErrText, errTxt);
+
+            
+//        Thread.sleep(5000);
+        
+//        String welcomeText = driver.findElement(By.xpath("//span[@class='title']")).getText();
+//        Assert.assertEquals("PRODUCTS", welcomeText);
         
 
         //Close the browser
-        driver.quit();
     }
     
     @BeforeTest
